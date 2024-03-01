1ec2128df9852fc8e1f784ef82c0c625142fb336
hoantd <taduchoan5503@gmail.com>
2024-03-01 09:22:53 +0700

    initial project

--- LuuTruVanThu_Project/Program.cs
+++ LuuTruVanThu_Project/Program.cs
@@ -14,6 +14,7 @@ namespace LuuTruVanThu_Project
         [STAThread]
         static void Main()
         {
+            // hello
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
