2aece0b73ddb3e266ccee50ee761cf771c83a349
hoantd <taduchoan5503@gmail.com>
2024-03-02 22:29:59 +0700

    database

---
+++ LuuTruVanThu_Project/DAO/DonViDAO.cs
@@ -0,0 +1,22 @@
+﻿using LuuTruVanThu_Project.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LuuTruVanThu_Project.DAO
+{
+    class DonViDAO
+    {
+        private static VanThuDBEntities _context = new VanThuDBEntities();
+        public static List<DonVis> GetData()
+        {
+            return _context.DonVis.ToList();
+        }
+        public bool CheckLogin(DonVis model, int Nam)
+        {
+            DonVis donVi = _context.DonVis.SingleOrDefault(model => model.)
+        }
+    }
+}
