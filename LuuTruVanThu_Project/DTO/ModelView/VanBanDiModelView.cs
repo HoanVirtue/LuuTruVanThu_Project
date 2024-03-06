cbc3a896a1df8284187efd1b5830da0652d99a9c
hoantd <taduchoan5503@gmail.com>
2024-03-07 01:19:18 +0700

    finish_vanbandi

---
+++ LuuTruVanThu_Project/DTO/ModelView/VanBanDiModelView.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace LuuTruVanThu_Project.DTO.ModelView
+{
+    internal class VanBanDiModelView
+    {
+        public string SoDi { get; set; }
+        public string SoKyHieu { get; set; }
+        public string TrichYeuNoiDung { get; set; }
+        public string LoaiVanBan { get; set; }
+        public string DonViBanHanh { get; set; }
+        public Nullable<System.DateTime> NgayBanHanh { get; set; }
+        public string NoiNhan { get; set; }
+        public Nullable<System.DateTime> NgayXuLy { get; set; }
+        public string TepTuLieu { get; set; }
+    }
+}
